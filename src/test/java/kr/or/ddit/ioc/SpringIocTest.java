e9b852cf930c4688b46cdcfaf131f9e00b53f01d
ena <zigedge@gmail.com>
2019-06-19 17:18:16 +0900

    spring 수업

---
+++ src/test/java/kr/or/ddit/ioc/SpringIocTest.java
@@ -0,0 +1,43 @@
+package kr.or.ddit.ioc;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.board.dao.IboardDao;
+import kr.or.ddit.board.service.IboardService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:kr/or/ddit/ioc/application-ioc-st.xml")
+public class SpringIocTest {
+	
+	@Resource(name="bDao")
+	private IboardDao boardDao;
+	
+	@Resource(name="bService")
+	private IboardService boardService;
+	
+	/**
+	 * 
+	* Method : test
+	* 작성자 : PC20
+	* 변경이력 :
+	* Method 설명 : 스프링 컨테이너 생성 테스트
+	 */
+	@Test
+	public void springIocTest() {
+		/***Given***/
+		
+
+		/***When***/
+
+		/***Then***/
+		assertEquals(boardDao, boardService.getBoardDao());
+	}
+
+}
