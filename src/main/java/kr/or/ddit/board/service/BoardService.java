bef9caa643d4af4849609f3125a758d80c4750ed
ena <zigedge@gmail.com>
2019-06-20 11:10:12 +0900

    spring container scope test  - singleton - prototype

--- src/main/java/kr/or/ddit/board/service/BoardService.java
+++ src/main/java/kr/or/ddit/board/service/BoardService.java
@@ -3,8 +3,17 @@ package kr.or.ddit.board.service;
 import kr.or.ddit.board.dao.IboardDao;
 
 public class BoardService implements IboardService {
+	// property or field
 	private IboardDao boardDao;
 	
+	// 인자를 받는 기본 생성자를 만들었다면 기본 생성자도 만들어야 오류가 안남
+	public BoardService() {
+	}
+
+	public BoardService(IboardDao boardDao) {
+		this.boardDao = boardDao;
+	}
+
 	@Override
 	public String sayHello() {
 		return boardDao.sayHello();
