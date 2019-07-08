614c4ba0d2099e6f281231cf2385ed13f38a3154
ena <zigedge@gmail.com>
2019-07-08 10:29:27 +0900

    Spring batch  - 문자열 처리 배치rangerBatch   rangerBatch   rangerBatchTest   scheduler 등록   rangerBatchScheduler 등록

--- src/test/java/kr/or/ddit/login/controller/LoginControllerTest.java
+++ src/test/java/kr/or/ddit/login/controller/LoginControllerTest.java
@@ -53,7 +53,7 @@ public class LoginControllerTest extends ControllerTestEnv{
 		ModelAndView mav = mvcResult.getModelAndView();
 		String viewName = mav.getViewName();
 		/***Then***/
-		assertEquals("main", viewName);
+		assertEquals("tiles.main", viewName);
 		
 	}
 	
@@ -83,7 +83,7 @@ public class LoginControllerTest extends ControllerTestEnv{
 		UserVo userVo = (UserVo)session.getAttribute("USER_INFO");
 		
 		/***Then***/
-		assertEquals("main", viewName);
+		assertEquals("tiles.main", viewName);
 		assertEquals("브라운", userVo.getName());
 		assertEquals("곰", userVo.getAlias());
 	}
